647cb55faa03c0dbb00f9c45ca8af5b3ea992e40
Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>
2016-03-08 20:11:11 -0600

    Add time comparison
    * Removing unused files
    * Adding pdf file
    * Modifying test generator
    
    Signed-off-by: Ulises Mendez Martinez <ulisesmdzmtz@gmail.com>

--- task1/gen.cpp
+++ task1/gen.cpp
@@ -1,17 +1,60 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cstdio>
+#include <cstdlib>
+#include <time.h>
 
-#define MAXN 123456
+#define MAXN 12345
+#define N 100000
 
 using namespace std;
 
+int v[N];
+
 int main()
 {
-	int test = 5;
 	srand(time(0));
+
+	// Ordered
+	printf("%d\n",N);
+	for(int i=0; i<N; i++)
+	{
+		printf("%d ",i+1);
+	}
+	puts("");
+
+	// Ordered Inversed
+	printf("%d\n",N);
+	for(int i=0; i<N; i++)
+	{
+		printf("%d ",N-i);
+	}
+	puts("");
+
+	// Almost ordered
+	for(int i=0; i<N; i++)
+	{
+		v[i] = i+1;
+	}
+	// Shuffle
+	for(int i=0; i<44; i++) //Some shuffle
+	{
+		int x=rand()%N, y=rand()%N;
+		swap(v[x],v[y]);
+	}
+	//Print
+	printf("%d\n",N);
+	for(int i=0; i<N; i++)
+	{
+		printf("%d ",v[i]);
+	}
+	printf("\n");
+
+	// Random
+	int test = 1;
 	while( test-- )
 	{
-		int N = rand() % MAXN + 1;
-		int v[N];
+		//int N = rand() % MAXN + 1;
+		//int v[N];
 		// Initialize
 		for(int i=0; i<N; i++)
 		{
@@ -20,7 +63,7 @@ int main()
 		// Shuffle
 		for(int i=0; i<N; i++)
 		{
-			for(int i=0; i<N; i++)
+			//for(int i=0; i<N; i++)
 			{
 				int x=rand()%N, y=rand()%N;
 				swap(v[x],v[y]);
